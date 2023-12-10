e54c9057460b090a9cf52a42310f09c4f1b3d97d
Twyggy <125589167+Twyggy@users.noreply.github.com>
2023-12-10 14:26:12 +0100

    ✅ thinker interface

--- molekulare-Genetik.py
+++ molekulare-Genetik.py
@@ -1,5 +1,5 @@
 import re 
-
+import tkinter as tk
 
 AS = {
     'UUU': 'Phe', 'UUC': 'Phe', 'UUA': 'Leu', 'UUG': 'Leu',
@@ -21,74 +21,101 @@ AS = {
 }
 
 
-mRNA = 'UGG AUG GGA CUA ACA UAA CUA'
+# mRNA = 'AUGGAGCCUCUGAAGGCCUGUGCUCAAGGGAGAAGUUGGAGGUGUGAAGAGCUGGGCAGUGUGAGGUGGUGGAAGAGCUGUGAGGCAGCAGUCUGAGGAGGUCUGUCCUCCUGGUGAUGUGGAGUAGCAGGAGGAAGAUGAGGUGGUGGUGGUGGUGGAAGAUGGUGGUGGAAGAGGAUGUGGAAGAUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGAUGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGAUGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUG'
 # 'AUGGAGCCUCUGAAGGCCUGUGCUCAAGGGAGAAGUUGGAGGUGUGAAGAGCUGGGCAGUGUGAGGUGGUGGAAGAGCUGUGAGGCAGCAGUCUGAGGAGGUCUGUCCUCCUGGUGAUGUGGAGUAGCAGGAGGAAGAUGAGGUGGUGGUGGUGGUGGAAGAUGGUGGUGGAAGAGGAUGUGGAAGAUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGAUGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGAUGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGUGGAAGGAGGUGGUGGUGGUGGUGGUGGUGGUG'
 # UGG AUG GGA CUA ACA UAA CUA UGG AUG GGA CUA ACA UAA CUA
-mRNA = mRNA.upper()
-splited = re.findall(r'\w{3}|\w+$', mRNA.upper())
+# mRNA = mRNA.upper()
+# splited = re.findall(r'\w{3}|\w+$', mRNA.upper())
 output = []
+lenght = 800
 
-def checker():
-    print('hello')
-
-for i in range(len(splited)):
-    last_stopp_index = 0
-    last_start_index = 0
-    
-    def translator(words):
-        if splited[i] in AS:
-            return [AS[word] for word in words.split()]
-        else:
-            return'Not found'
-
-    # output.append(translator(splited[i]))
-    # Suche nach aufeinanderfolgenden dreibuchstabigen Wörtern, gefolgt von optionalen einzelnen oder mehreren Buchstaben am Ende
-    # print(translator(splited[i]))
-    objected = translator(splited[i])
-    stringified = objected[0]
-    if 'stopp' in output:
-        last_stopp_index = len(output) - output[::-1].index('stopp') 
-    if 'start' in output:
-        last_start_index = len(output) - output[::-1].index('start')
-    
-    print(last_start_index, last_stopp_index)
-
-    if splited[i] == 'AUG':
-        print('catch')
-        if last_start_index <= last_stopp_index:
-            stringified = AS['AUG'][1]
-        elif last_stopp_index > last_start_index:
-            stringified = AS['AUG'][1]
-        elif last_stopp_index < last_start_index:
-            stringified = AS['AUG'][0]
-    
-    # wenn man if-Schleife verwendet, werden nur alle sachen zweichen start und stopp gespoted…
-    if last_start_index > last_stopp_index or splited[i] == 'AUG':
-        output.append(stringified)
-        if stringified == 'stopp':
-            output.append(' ')
+def translate():
+    mRNA = entry.get()
+    mRNA = mRNA.upper()
+    splited = re.findall(r'\w{3}|\w+$', mRNA.upper())
+    for i in range(len(splited)):
+        last_stopp_index = 0
+        last_start_index = 0
         
-print(output)
+        def translator(words):
+            if splited[i] in AS:
+                return [AS[word] for word in words.split()]
+            else:
+                return'Not found'
 
-# gibt den index von start und stopp
-start_indices = [i for i, x in enumerate(output) if x == 'start']
-stop_indices = [i for i, x in enumerate(output) if x == 'stopp']
+        # output.append(translator(splited[i]))
+        # Suche nach aufeinanderfolgenden dreibuchstabigen Wörtern, gefolgt von optionalen einzelnen oder mehreren Buchstaben am Ende
+        # print(translator(splited[i]))
+        objected = translator(splited[i])
+        stringified = objected[0]
+        if 'stopp' in output:
+            last_stopp_index = len(output) - output[::-1].index('stopp') 
+        if 'start' in output:
+            last_start_index = len(output) - output[::-1].index('start')
+        
+        print(last_start_index, last_stopp_index)
 
-print(start_indices, stop_indices)
+        if splited[i] == 'AUG':
+            print('catch')
+            if last_start_index <= last_stopp_index:
+                stringified = AS['AUG'][1]
+            elif last_stopp_index > last_start_index:
+                stringified = AS['AUG'][1]
+            elif last_stopp_index < last_start_index:
+                stringified = AS['AUG'][0]
+        
+        # wenn man if-Schleife verwendet, werden nur alle sachen zweichen start und stopp gespoted…
+        if last_start_index > last_stopp_index or splited[i] == 'AUG':
+            output.append(stringified)
+            if stringified == 'stopp':
+                output.append(' ')
+            
+    print(output)
 
-result = []
+    # gibt den index von start und stopp
+    start_indices = [i for i, x in enumerate(output) if x == 'start']
+    stop_indices = [i for i, x in enumerate(output) if x == 'stopp']
 
-# filtert, was zwischen start und stopp steht
-for start, stop in zip(start_indices, stop_indices):
-    if start < stop:
-        result.append(output[start+1:stop])
+    print(start_indices, stop_indices)
 
-# print(result)
+    result = []
 
-result_strings = [' '.join(sublist) for sublist in result]
-result_combined = ''' | '''.join(result_strings)
+    # filtert, was zwischen start und stopp steht
+    for start, stop in zip(start_indices, stop_indices):
+        if start < stop:
+            result.append(output[start+1:stop])
 
-print('AS sequence: ', result_combined)
-# # outputstr = ' '.join(output) | wenn man stopp und start sehen möchte
-# outputstr = ' '.join(item for item in output if item not in ('start', 'stopp'))
-# print('Amino acid sequence:', outputstr)
+    # print(result)
+
+    result_strings = [' - '.join(sublist) for sublist in result]
+    result_combined = '''
+'''.join(result_strings)
+
+    print(result)
+    print('AS sequence: ', result_combined)
+    result_text.set(result_combined)
+    # outputstr = ' '.join(output) | wenn man stopp und start sehen möchte
+    # outputstr = ' '.join(item for item in output if item not in ('start', 'stopp'))
+    # print('Amino acid sequence:', outputstr)
+    
+    
+root = tk.Tk()
+root.title("mRNA Translator")
+
+frame = tk.Frame(root)
+frame.pack(padx=10, pady=10)
+
+label = tk.Label(frame, text="Enter mRNA sequence:")
+label.grid(row=0, column=0, pady=200 )
+
+entry = tk.Entry(frame, width=100)
+entry.grid(row=0, column=1, padx=5, pady=5)
+
+translate_button = tk.Button(frame, text="Translate", command=translate)
+translate_button.grid(row=1, column=0, columnspan=2, pady=0)
+
+result_text = tk.StringVar()
+result_label = tk.Label(frame, textvariable=result_text, wraplength=lenght, justify=tk.LEFT, width=100)
+result_label.grid(row=2, column=0, columnspan=2)
+
+root.mainloop()
